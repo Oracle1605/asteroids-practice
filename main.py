cb01e434db3f1961af4a7a1dbfe0740ba8e8c49c
Oracle1605 <mkrunek@gmail.com>
2026-02-12 20:11:25 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame # type: ignore
+from constants import SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}, Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+               return
+        screen.fill("black")
+        pygame.display.flip()
+        
+if __name__ == "__main__":
+    main()
