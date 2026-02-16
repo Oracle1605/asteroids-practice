4de3c4e73a233f3372e8bc601aa7e360ca7de3c8
Oracle1605 <mkrunek@gmail.com>
2026-02-15 20:54:19 -0600

    Updated Player.py to enable movement, and rotation with w,a,s,d

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import pygame # type: ignore
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 def main():
     pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}, Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    log_state()
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                return
         screen.fill("black")
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+        log_state()
+        dt =clock.tick(60) / 1000
         
 if __name__ == "__main__":
     main()
